8b2bb7e720c8ce5158291dd5ae26a4ce9264cdfe
Git-Yogurt <vm@li.cm>
2016-08-10 09:08:33 +0800

    fragment2

---
+++ AndroidStudioProjects/LearnFragment/app/src/main/java/com/android/yogurt/learnfragment/Fragment2.java
@@ -0,0 +1,20 @@
+package com.android.yogurt.learnfragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Yogurt on 16/8/10.
+ */
+public class Fragment2 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView=inflater.inflate(R.layout.fragment2,container,false);
+        return rootView;
+    }
+}
